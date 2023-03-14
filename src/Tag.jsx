0a11950cd0467ed7150e03c23084183e2d6817c7
JT5D <jtunick@theimclab.com>
2023-03-14 00:57:12 -0400

    latest

--- src/Tag.jsx
+++ src/Tag.jsx
@@ -5,20 +5,20 @@ export default function Tag() {
   return (
     <div className="copy">
       <span>
-        <a target="_blank" href="https://github.com/pmndrs/lamina">
+        <a target="_blank" href="https://github.com/jt5d/3dgrassball">
           <Logo />
         </a>
       </span>
 
       <span>
         Made with 🧡 by{' '}
-        <a target="_blank" href="https://twitter.com/CantBeFaraz">
+        <a target="_blank" href="https://twitter.com/TheIMCLab">
           Faraz Shaikh
         </a>
       </span>
 
       <span>
-        <a target="_blank" href="https://github.com/FarazzShaikh">
+        <a target="_blank" href="https://github.com/imclab">
           <FaGithub size={40} />
         </a>
       </span>
